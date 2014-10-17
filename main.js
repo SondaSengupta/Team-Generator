e01a2ac1d42cbfc20a34407ffe2b58632a2cc5e7
SondaSengupta <sondasengupta@gmail.com>
2014-10-17 15:21:06 -0500

    Changed the if else statements into one switch function

--- main.js
+++ main.js
@@ -4,17 +4,25 @@ document.addEventListener('DOMContentLoaded', function(){
 		}
 
 	document.getElementById('submit').addEventListener("click", function(){
-			if (teamValue() ==="randomstudent") {
-				alert("You clicked the 1st selection of " + teamValue());
-			} else if (teamValue() ==="neighboringpair") {
-				alert("You clicked the 2nd selection of " + teamValue());
-			} else if (teamValue() ==="randompair") {
-				alert("You clicked the 3rd selection of " + teamValue());
-			} else if (teamValue() ==="randomthree") {
-				alert("You clicked the 4th selection of " + teamValue());
-			} else if (teamValue() ==="randomnpair") {
-				alert("You clicked the 5th selection of " + teamValue());
-			}
+			switch (teamValue()) {
+				case "randomstudent":
+					alert("You clicked the 1st selection of " + teamValue());
+					break;
+				case "neighboringpair":
+					alert("You clicked the 2nd selection of " + teamValue());
+					break;
+				case "neighboringthree":
+					alert("You clicked the 3rd selection of " + teamValue());
+					break;
+				case "randompair":
+					alert("You clicked the 4th selection of " + teamValue());
+					break;
+				case "randomthree":
+					alert("You clicked the 5th selection of " + teamValue());
+					break;
+				case "randomnpair":
+					alert("You clicked the 6th selection of " + teamValue());
+					break;
+				}
 		});
 });
-
