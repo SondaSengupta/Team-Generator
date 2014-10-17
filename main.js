04edf8880eeae8f425ce7eaf4eed43167eb8ab27
SondaSengupta <sondasengupta@gmail.com>
2014-10-17 15:06:29 -0500

    Added if else statements for specific alerts

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ document.addEventListener('DOMContentLoaded', function(){
 		}
 
 	document.getElementById('submit').addEventListener("click", function(){
-			alert("You clicked the button " + teamValue());
-		}); 
+			if (teamValue() ==="randomstudent") {
+				alert("You clicked the 1st selection of " + teamValue());
+			} else if (teamValue() ==="neighboringpair") {
+				alert("You clicked the 2nd selection of " + teamValue());
+			} else if (teamValue() ==="randompair") {
+				alert("You clicked the 3rd selection of " + teamValue());
+			} else if (teamValue() ==="randomthree") {
+				alert("You clicked the 4th selection of " + teamValue());
+			} else if (teamValue() ==="randomnpair") {
+				alert("You clicked the 5th selection of " + teamValue());
+			}
+		});
 });
+
