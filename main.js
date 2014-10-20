582b88dde525e2eacbe9e508b74bcf958a3ce56a
SondaSengupta <sondasengupta@gmail.com>
2014-10-20 14:18:34 -0500

    Added random pair and shuffling function

--- main.js
+++ main.js
@@ -14,40 +14,53 @@ document.addEventListener('DOMContentLoaded', function(){
     $li.innerHTML = items;
     $list.appendChild($li);
 }
+	function neighborGrouping(list,groupSize, target){
+		var listClone = list.slice(0);
+		while( listClone.length > 0 ){
+        	var studentNames = listClone.splice(0, groupSize);
+        	addItemToList(target, studentNames.join(" &amp; "));
+		}
+	}
+
+	function arrayShuffle(array) {
+		var arrayClone = array.slice(0);
+		var temp;
+			for (var i = 0; i < arrayClone.length; i++) {
+				var rand = getRandomInt(0, arrayClone.length);
+				temp = arrayClone[i]
+				arrayClone[i] = arrayClone[rand];
+				arrayClone[rand] = temp;
+			}
+			return arrayClone;
+	}
 
 	document.getElementById('submit').addEventListener("click", function(){
 			event.preventDefault();
-        	var $ul = document.getElementById("results");
-       		 $ul.innerHTML = "";
+        	var $ol = document.getElementById("results");
+       		 $ol.innerHTML = "";
 			
 			switch (teamValue()) {
 				case "randomstudent":
 				    var rand = getRandomInt(0, people.length);
 				    var person = people[rand];
-					event.preventDefault();
-					addItemToList($ul, person);
+					addItemToList($ol, person);
 					break;
 
 				case "neighboringpair":
-					var peopleClone = people.slice(0);
-					while( peopleClone.length > 0 ){
-                		var studentNames = peopleClone.splice(0, 2);
-                		addItemToList($ul, studentNames.join(" &amp; "));
-					}
+					neighborGrouping (people, 2, $ol)
 					break;
 
 
 				case "neighboringthree":
-					var peopleClone = people.slice(0);
-					while( peopleClone.length > 0 ){
-                		var studentNames = peopleClone.splice(0, 3);
-                		addItemToList($ul, studentNames.join(" &amp; "));
-					}
+					neighborGrouping (people, 3, $ol)
 					break;
-					
+
 				case "randompair":
-					alert("You clicked the 4th selection of " + teamValue());
+					var shuffledPeople = arrayShuffle(people);
+					neighborGrouping (shuffledPeople,2, $ol)
 					break;
+
+
 				case "randomthree":
 					alert("You clicked the 5th selection of " + teamValue());
 					break;
